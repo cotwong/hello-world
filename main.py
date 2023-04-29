dc98cb36ae3676a7d946890dd5beb7413f4821e9
Flora Huang <yufeih@uchicago.edu>
2023-04-29 12:28:13 -0700

    Change response

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    name = os.environ.get("NAME", "there")
+    name = os.environ.get("NAME", "world")
     return "Hello {}!".format(name)
 
 
