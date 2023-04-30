8cc3aa003a0efc1dbf7a6eefc52c1127fbc3805d
Flora Huang <yufeih@uchicago.edu>
2023-04-30 11:51:36 -0700

    Change environment settings

--- db.py
+++ db.py
@@ -1,24 +1,33 @@
 import os
 import pymysql
-from flask import Flask
-
-app = Flask(__name__)
+from flask import jsonify
 
 db_user = os.environ.get('cotwong')
 db_password = os.environ.get('qwe123')
 db_name = os.environ.get('mydatabase')
-db_host = os.environ.get('cloudwerx-assessment:us-central1:user-db')
+db_connection_name = os.environ.get('cloudwerx-assessment:us-central1:user-db')
 
-@app.route('/')
+
+def open_connection():
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    try:
+        if os.environ.get('GAE_ENV') == 'standard':
+            conn = pymysql.connect(user=db_user,
+                                   password=db_password,
+                                   unix_socket=unix_socket,
+                                   db=db_name,
+                                   cursorclass=pymysql.cursors.DictCursor
+                                   )
+    except pymysql.MySQLError as e:
+        return e
+    return conn
+
+
 def hello_world():
-    conn = pymysql.connect(
-        user=db_user,
-        password=db_password,
-        database=db_name,
-        host=db_host
-    )
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM users')
-    results = cursor.fetchall()
-    conn.close()
-    return str(results)
+    conn = open_connection()
+    with conn.cursor() as cursor:
+        cursor.execute('SELECT * FROM my_table')
+        results = cursor.fetchall()
+        conn.commit()
+        conn.close()
+        return results
