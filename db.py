69519a75ac41dd299b9c7069c900be26afc77916
Flora Huang <yufeih@uchicago.edu>
2023-04-30 12:40:44 -0700

    Update database connection

--- db.py
+++ db.py
@@ -1,6 +1,5 @@
 import os
 import pymysql
-from flask import jsonify
 
 db_user = os.environ.get('cotwong')
 db_password = os.environ.get('qwe123')
@@ -8,28 +7,18 @@ db_name = os.environ.get('mydatabase')
 db_connection_name = os.environ.get('cloudwerx-assessment:us-central1:user-db')
 
 
-def open_connection():
-    global conn
-    unix_socket = '/cloudsql/{}'.format(db_connection_name)
-    try:
-        if os.environ.get('GAE_ENV') == 'standard':
-            conn = pymysql.connect(user=db_user,
-                                   password=db_password,
-                                   unix_socket=unix_socket,
-                                   db=db_name,
-                                   cursorclass=pymysql.cursors.DictCursor
-                                   )
-    except pymysql.MySQLError as e:
-        return e
-    return conn
-
-
 def get():
-    conn = open_connection()
-    with conn.cursor() as cursor:
-        cursor.execute('SELECT * FROM my_table')
-        results = cursor.fetchall()
-        conn.commit()
-        conn.close()
-        return results
+    unix_socket = '/cloudsql/{}'.format(db_connection_name)
+    conn = pymysql.connect(
+        user=db_user,
+        password=db_password,
+        unix_socket=unix_socket,
+        db=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM mytable')
+    results = cursor.fetchall()
+    conn.close()
+    return str(results)
 
