c18c3f2d554c7d67d1d3585f622fddb5671d9556
Flora Huang <yufeih@uchicago.edu>
2023-04-30 12:15:55 -0700

    Update database connection

--- db.py
+++ db.py
@@ -1,14 +1,15 @@
 import os
 import pymysql
-from flask import Flask, jsonify
+from flask import jsonify
 
-db_user = os.environ.get('root')
+db_user = os.environ.get('cotwong')
 db_password = os.environ.get('qwe123')
 db_name = os.environ.get('mydatabase')
 db_connection_name = os.environ.get('cloudwerx-assessment:us-central1:user-db')
 
 
 def open_connection():
+    global conn
     unix_socket = '/cloudsql/{}'.format(db_connection_name)
     try:
         if os.environ.get('GAE_ENV') == 'standard':
@@ -31,3 +32,4 @@ def get():
         conn.commit()
         conn.close()
         return results
+
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import os
-
 from flask import Flask
 from db import get
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def hello_world():
     return get()
+
 
 if __name__ == "__main__":
     app.run()
