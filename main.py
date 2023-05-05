ffcf8fad7cfbce7919e2ee3f0b26f29605de9211
Flora Huang <yufeih@uchicago.edu>
2023-05-05 01:56:04 -0700

    Change database settings

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
-import os
-from flask import Flask
-from db import get
+from flask import Flask, jsonify
+import pymysql
 
 app = Flask(__name__)
 
+# Cloud SQL database configuration
+db_user = 'yufeih11'
+#db_password = ''
+db_name = 'demo-db'
+cloud_sql_connection_name = 'cloudwerx-assessment:us-central1:demo'
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return get()
+
+# Connect to Cloud SQL database
+def get_db():
+    return pymysql.connect(
+        user=db_user,
+       # password=db_password,
+        host='127.0.0.1',
+        port=3306,
+        database=db_name,
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 80)))
+@app.route('/')
+def hello():
+    conn = get_db()
+
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT * FROM users")
+
+        result = cursor.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    # Return the query results as JSON
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
