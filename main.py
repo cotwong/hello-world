82e2fb01e9c1e7c143bfa0a05ad31cd8b375dc35
Flora Huang <yufeih@uchicago.edu>
2023-05-05 10:30:17 -0700

    Change yaml settings

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ cloud_sql_connection_name = 'cloudwerx-assessment:us-central1:demo'
 def get_db():
     return pymysql.connect(
         user=db_user,
-        # host='127.0.0.1',
+        host='127.0.0.1',
         #host='0.0.0.0',
         port=3306,
         database=db_name,
